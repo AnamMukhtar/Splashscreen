e9b893f0e2b9797ede460530c1be364ef0096ba0
AnamMukhtar <anamm4050@gmail.com>
2018-10-25 02:04:55 +0500

    Initial commit

---
+++ app/src/main/java/com/example/anam/splashscreen/home.java
@@ -0,0 +1,17 @@
+package com.example.anam.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class home extends AppCompatActivity {
+private static int Splash=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
